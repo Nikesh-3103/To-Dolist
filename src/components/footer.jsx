12f28ab9434e91b80c75665c836d73cbee5ef8a4
Nikesh-3103 <nikeshkumar310305@gmail.com>
2025-04-19 03:16:03 +0530

    footer added

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 export function Footer() {
     return (
       <footer className="footer">
-        <p>Built by <a href="https://nick031.netlify.app/">Nikesh</a></p>
+        <p>Built by <a href="https://nick031.netlify.app/" target="_blank" rel="noopener noreferrer">Nikesh</a></p>
       </footer>
     );
   }
